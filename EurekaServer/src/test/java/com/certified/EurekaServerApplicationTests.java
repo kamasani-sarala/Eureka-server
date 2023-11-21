c5a38e2f9f0a8a4b98b70bf7d3b95d389b0d5d18
kamasani-sarala <105160074+kamasani-sarala@users.noreply.github.com>
2023-11-21 10:51:53 +0530

    Add files via upload

---
+++ EurekaServer/src/test/java/com/certified/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.certified;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
